fadc206e4466316bcc977dbb7758c2fdd1b6028d
Arpita <arpitakumari10653@gmail.com>
2025-05-21 20:19:07 +0530

    doing negative slicing

---
+++ 02_negative_slicing.py
@@ -0,0 +1,10 @@
+name = "arpita"
+
+print(name[0:4])
+
+print(name[-4:-1])
+print(name[2:5])    #here u can make change negative to positive then do slicing
+
+print(name[:4]) #is same as print(name[0:4])
+print(name[1:]) #is same as print(name[1:6]) 
+print(name[1:6])
